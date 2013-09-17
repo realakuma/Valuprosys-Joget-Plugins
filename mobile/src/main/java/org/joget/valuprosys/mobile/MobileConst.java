007ddaa471ee32ce2c6e0e0de494ce8d3e221647
realakuma <akuma.zhang@gmail.com>
2013-09-17 21:48:54 +0800

    add consts
    add consts

--- mobile/src/main/java/org/joget/valuprosys/mobile/MobileConst.java
+++ mobile/src/main/java/org/joget/valuprosys/mobile/MobileConst.java
@@ -9,25 +9,26 @@ package org.joget.valuprosys.mobile;
  * @author akuma
  */
 public class MobileConst {
-    
-    public static String Approver="_approver";
-    public static String Comment="_comment";
-    public static String CompleteWithVariable="completeWithVariable";
-    public static String GetApprovementHistoryList="getApprovementHistoryList";
-    public static String GetAssignmentPendingAndAcceptedList="getAssignmentPendingAndAcceptedList";
-    public static int getrows=100;
-    public static String holiday="请假单";
-    public static String in_turn="调假单";
-    public static String b_t="出差单";
-    public static String leaveType="leavetype";
-    public static String wowApiGetEmp="http://10.0.10.122:88/GetEmp";
-    public static String wowApiGetOrg="http://10.0.10.122:88/GetOrg";
-    public static String subFormPoDetail="subform_po_detail";
-    public static String subFormPoDetailUrl="custom_jw/product_subfrom.jsp?id=";
-    public static String subFormFeeDetail="subform_fee_detail";
-    public static String subFormFeeDetailUrl="custom_jw/expense_subfrom.jsp?id=";
-    public static String Andriod="Andriod";
-    public static String IOS="IOS";
-    public static String column_prefix="c_";
-    
+
+    public static String Approver = "_approver";
+    public static String Comment = "_comment";
+    public static String expenseId = "expenseId";
+    public static String productId = "productId";
+    public static String CompleteWithVariable = "completeWithVariable";
+    public static String GetApprovementHistoryList = "getApprovementHistoryList";
+    public static String GetAssignmentPendingAndAcceptedList = "getAssignmentPendingAndAcceptedList";
+    public static int getrows = 100;
+    public static String holiday = "请假单";
+    public static String in_turn = "调假单";
+    public static String b_t = "出差单";
+    public static String leaveType = "leavetype";
+    public static String wowApiGetEmp = "http://10.0.10.122:88/GetEmp";
+    public static String wowApiGetOrg = "http://10.0.10.122:88/GetOrg";
+    public static String subFormPoDetail = "subform_po_detail";
+    public static String subFormPoDetailUrl = "custom_jw/product_subfrom.jsp?id=";
+    public static String subFormFeeDetail = "subform_fee_detail";
+    public static String subFormFeeDetailUrl = "custom_jw/expense_subfrom.jsp?id=";
+    public static String Andriod = "Andriod";
+    public static String IOS = "IOS";
+    public static String column_prefix = "c_";
 }
